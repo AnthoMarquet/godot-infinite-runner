6b50c8a4b240a107066ee19f1a5efbe95b9cd75a
Anthony MARQUET <anthony.marquet@zenika.com>
2024-02-27 17:12:02 +0100

    feat: create folders and rename files

--- scripts/Corridor.gd
+++ scripts/Corridor.gd
@@ -3,24 +3,20 @@ class_name Corridor
 
 var number = 3
 var offset = 100
-var count = 0
-
-## The set of terrian blocks which are currently rendered to viewport
-var terrain_belt: Array[MeshInstance3D] = []
 
 func _ready():
 	_initGround()
 	
 func _initGround():
 	for n in number:
-		var ground: Ground = preload("res://ground.tscn").instantiate()
+		var ground: Ground = preload("res://scenes/Ground.tscn").instantiate()
 		ground.position.z = -(n*offset)
 		ground.position.y = 0
 		ground.position.x = 0
 		add_child(ground)
 		
 func _addGroundAtTheEndOfCorridor(position):
-		var ground: Ground = preload("res://ground.tscn").instantiate()
+		var ground: Ground = preload("res://scenes/Ground.tscn").instantiate()
 		ground.position.z = position-(number*offset)
 		ground.position.y = 0
 		ground.position.x = 0
